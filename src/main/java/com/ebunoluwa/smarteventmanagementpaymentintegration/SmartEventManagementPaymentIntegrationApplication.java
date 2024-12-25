e0ddda5a4b6448fa8ff5019db0de710f5a91b9d7
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2024-12-25 16:59:00 +0100

    First commit

---
+++ src/main/java/com/ebunoluwa/smarteventmanagementpaymentintegration/SmartEventManagementPaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.ebunoluwa.smarteventmanagementpaymentintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEventManagementPaymentIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEventManagementPaymentIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebunoluwa/smarteventmanagementpaymentintegration/SmartEventManagementPaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebunoluwa.smarteventmanagementpaymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEventManagementPaymentIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
